db0617d45babc18d2a9696f942af97e3e5430404
ibrahem Qasim <ibrahem.al-radaei@outlook.com>
2023-10-31 21:54:12 +0300

    Update up.js

--- public_html/mass/js/up.js
+++ public_html/mass/js/up.js
@@ -68,11 +68,39 @@ function idElement_err(idElement, err) {
     idElement.text(err);
     idElement.css({ "font-weight": "bold", "color": "#f53333" });
 }
+
+function start_up(file, id) {
+    var idElement = $("#" + id);
+    idElement.html('<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Uploading..');
+    // console.log(file)
+    //---
+    publishnew(file, function (err, data) {
+        console.log(data);
+        // if err or not data
+        if (err || !data) {
+            //{    "error": "[api] Received error :- invalidtitle : Bad title \"File:\"."}
+            idElement_err(idElement, 'false' + err);
+        } else {
+            var error = data.error;
+            if (error) {
+                idElement_err(idElement, 'false' +  data.error);
+            } else {
+                var result = data.result;
+                if (result == "Success") {
+                    $('#name_' + id).html('<a href="https://nccommons.org/wiki/File:' + file.name + '" target="_blank">' + file.name + '</a>');
+                    idElement.text('true');
+                    idElement.css({ "color": "#45f533", "font-weight": "bold" });
+                } else {
+                    idElement_err(idElement, 'false' + result);
+                }
+            }
+        }
+    });
+    
+}
 function upload_f(file, id) {
     var idElement = $("#" + id);
     idElement.html('<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Checking...');
-    //---
-    var not_exists = false;
     //---
     check_image_exist(file.name, function (exists, notexists, title) {
         if (exists) {
@@ -81,42 +109,14 @@ function upload_f(file, id) {
             idElement.css({ "color": "#f53333", "font-weight": "bold" });
         } else {
             if (notexists) {
-                not_exists = true;
+                start_up(file, id);
                 //---
             } else {
-                idElement_err(idElement, 'Error..');
+                idElement.text('Error..');
+                idElement.css({ "font-weight": "bold", "color": "#f53333" });
             }
         }
     });
-    //---
-    if (not_exists) {
-        idElement.html('<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Uploading..');
-        // console.log(file)
-        //---
-        publishnew(file, function (err, data) {
-            console.log(data);
-            // if err or not data
-            if (err || !data) {
-                //{    "error": "[api] Received error :- invalidtitle : Bad title \"File:\"."}
-                idElement_err(idElement, 'false' + err);
-            } else {
-                var error = data.error;
-                if (error) {
-                    idElement_err(idElement, 'false' +  data.error);
-                } else {
-                    var result = data.result;
-                    if (result == "Success") {
-                        $('#name_' + id).html('<a href="https://nccommons.org/wiki/File:' + file.name + '" target="_blank">' + file.name + '</a>');
-                        idElement.text('true');
-                        idElement.css({ "color": "#45f533", "font-weight": "bold" });
-                    } else {
-                        idElement_err(idElement, 'false' + result);
-                    }
-                }
-            }
-        });
-    }
-
 }
 
 $(document).ready(function () {
