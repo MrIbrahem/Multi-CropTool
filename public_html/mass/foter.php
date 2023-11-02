10179dd6852b13ea1bbe032d8fd2e7d485476432
ibrahem Qasim <ibrahem.al-radaei@outlook.com>
2023-11-02 13:46:52 +0300

    Update foter.php

--- public_html/mass/foter.php
+++ public_html/mass/foter.php
@@ -2,5 +2,24 @@
 </main>
 
 </body>
+<script>
+	function login() {
+		var cat = $('#cat').val() || '';
+		var code = $('#code').val() || '';
+		var type = $('input[name=type]:checked').val() || '';
 
+		var url = 'login.php?action=login';
+		if (cat !== '') {
+			url += '&cat=' + cat;
+		}
+		if (code !== '') {
+			url += '&code=' + code;
+		}
+		if (type !== '') {
+			url += '&type=' + type;
+		}
+
+		window.location.href = url;
+	}
+</script>
 </html>
