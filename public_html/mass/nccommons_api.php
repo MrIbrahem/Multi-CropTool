f6588c5f1f69856dfd4116253d5002fea7c7fb5f
ibrahem Qasim <ibrahem.al-radaei@outlook.com>
2023-11-04 13:38:59 +0300

    Update public_html/mass/nccommons_api.php
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- public_html/mass/nccommons_api.php
+++ public_html/mass/nccommons_api.php
@@ -1,4 +1,10 @@
 <?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  if (!isset($_POST['csrf_token']) || $_POST['csrf_token'] !== $_SESSION['csrf_token']) {
+    // CSRF token is invalid
+    die('Invalid CSRF token');
+  }
+}
 $data = $_POST;
 $api = 'https://nccommons.org/w/api.php';
 // post data to api
@@ -8,5 +14,9 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
 $result = curl_exec($ch);
+if (curl_errno($ch)) {
+  // handle error, e.g. log error message
+  error_log('cURL error: ' . curl_error($ch));
+}
 curl_close($ch);
 echo $result;
