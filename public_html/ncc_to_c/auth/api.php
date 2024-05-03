f3f177bf066548e00be5cc515a1b727e3e141a01
ibrahem <ibrahem.al-radaei@outlook.com>
2024-05-03 03:14:48 +0300

    .

---
+++ public_html/ncc_to_c/auth/api.php
@@ -0,0 +1,156 @@
+<?php
+//---
+if (isset($_REQUEST['test'])) {
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+};
+//---
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use MediaWiki\OAuthClient\Client;
+use MediaWiki\OAuthClient\ClientConfig;
+use MediaWiki\OAuthClient\Consumer;
+use MediaWiki\OAuthClient\Token;
+
+// Output the demo as plain text, for easier formatting.
+header('Content-type: text/plain');
+
+// Get the wiki URL and OAuth consumer details from the config file.
+require_once __DIR__ . '/config.php';
+
+// Configure the OAuth client with the URL and consumer details.
+$conf = new ClientConfig($oauthUrl);
+$conf->setConsumer(new Consumer($consumerKey, $consumerSecret));
+$conf->setUserAgent($gUserAgent);
+$client = new Client($conf);
+
+// Load the Access Token from the session.
+session_start();
+$accessToken = new Token($_SESSION['access_key'], $_SESSION['access_secret']);
+
+// Example 1: get the authenticated user's identity.
+$ident = $client->identify($accessToken);
+
+function get_edit_token()
+{
+    global $client, $accessToken, $apiUrl;
+    // Example 3: make an edit (getting the edit token first).
+    $editToken = json_decode($client->makeOAuthCall(
+        $accessToken,
+        "$apiUrl?action=query&meta=tokens&format=json"
+    ))->query->tokens->csrftoken;
+    //---
+    return $editToken;
+}
+
+function doApiQuery($Params, $addtoken = null)
+{
+    global $client, $accessToken, $apiUrl;
+    //---
+    if ($addtoken !== null) $Params['token'] = get_edit_token();
+    //---
+    $Result = $client->makeOAuthCall(
+        $accessToken,
+        $apiUrl,
+        true,
+        $Params
+    );
+    //---
+    return json_decode($Result, true);
+}
+
+function doEdit($data) {
+    return doApiQuery($data, $addtoken = true);
+}
+
+
+function upload($post) {
+    $url = $post['url'] ?? '';
+    $file = $_FILES['file'];
+
+    // Validate and sanitize other inputs if needed
+    $filename = filter_var($post['filename'] ?? '', FILTER_SANITIZE_STRING);
+    $comment = filter_var($post['comment'] ?? '', FILTER_SANITIZE_STRING);
+
+    //---
+
+    $data = [
+        'action' => 'upload',
+        'format' => 'json',
+        'filename' => $filename,
+        'comment' => $comment,
+    ];
+
+    //---
+
+    if ($url == '' && $file == '') {
+        $err = ["error" => "Invalid", "filename" => $filename, "url" => $url];
+        echo json_encode($err);
+        return;
+    }
+
+    //---
+
+    if ($url != '') {
+        $data['url'] = $url;
+    } else {
+        // CURLFile
+        $data['file'] = new \CURLFile($file['name'], $file['type'], $file['tmp_name'], $file['size']);
+    }
+
+    //---
+
+    $uu = doEdit($data);
+
+    //---
+
+    echo json_encode($uu);
+}
+
+
+function find_exists() {
+    // Sanitize the filename to prevent malicious code injection
+    $sanitizedFilename = filter_var($_GET['filename'], FILTER_SANITIZE_STRING);
+    $filename = $sanitizedFilename ?? '';
+
+    $params = [
+        'action' => 'query',
+        'format' => 'json',
+        'formatversion' => '2',
+        'titles' => "File:" . $filename
+    ];
+
+    //---
+    $res = doEdit($params);
+    //---
+    // Check if $res contains the specific phrase "authorized this application yet"
+    if (is_string($res) && $res == 'login') {
+        // echo "login";
+        return;
+    }
+    //---
+    // { "batchcomplete": true, "query": { "normalized": [ { "fromencoded": false, "from": "File:IMG_20220107_153333.jpg", "to": "File:IMG 20220107 153333.jpg" } ], "pages": [ { "pageid": 1190645, "ns": 6, "title": "File:IMG 20220107 153333.jpg" } ] } }
+    // echo json_encode($res);
+    $pages = $res['query']['pages'][0] ?? null;
+    //---
+    $result = ["exists" => "false"];
+    //---
+    if ($pages && !isset($pages['missing'])) {
+        $result['exists'] = "true";
+    };
+    //---
+    echo json_encode($result);
+}
+
+
+//---
+switch ($_REQUEST['do'] ?? '') {
+    case 'upload':
+        upload($_REQUEST);
+        break;
+    case 'exists':
+        find_exists();
+        break;
+}
+//---
---
+++ public_html/ncc_to_c/js/info.js
@@ -0,0 +1,166 @@
+
+function idElement_err(idElement, err) {
+    idElement.text(err);
+    idElement.addClass("text-danger");
+    idElement.css({ "font-weight": "bold" });
+}
+
+async function save_it(file, id) {
+    return new Promise((resolve, reject) => {
+        // save file to server
+        //---
+        $("#save_" + id).html('<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Saving...');
+        //---
+        // Create a FormData object to send files
+        const formData = new FormData();
+        formData.append('file', file);
+
+        let attempt = 0;
+
+        // Define a function to handle the fetch operation
+        const sendRequest = () => {
+            attempt++;
+            fetch('save.php', {
+                method: 'POST',
+                body: formData
+            })
+                .then(response => {
+                    if (response.ok) {
+                        return response.text();
+                    }
+                    throw new Error('Network response was not ok.');
+                })
+                .then(data => {
+                    if (data.trim() === 'true') {
+                        $("#save_" + id).html('<span class="bi bi-check2"></span> <a href="/mass/files/' + file + '" target="_blank">Saved!</a>');
+                        resolve(true); // Resolves the Promise with true if saved successfully
+                    } else {
+                        if (attempt < 3) {
+                            $("#save_" + id).html('<span class="bi bi-x"></span> Error! (Attempt ' + attempt + ') Retrying...');
+                            // Retry after 1 second in case of an error
+                            setTimeout(sendRequest, 1000); // Retry after 1 second
+                        } else {
+                            $("#save_" + id).html('<span class="bi bi-x"></span> Error! Maximum attempts reached');
+                            reject(false); // Rejects the Promise with false after maximum attempts
+                        }
+                    }
+                })
+                .catch(error => {
+                    console.error('There was a problem with the fetch operation:', error);
+                    if (attempt < 3) {
+                        $("#save_" + id).html('<span class="bi bi-x"></span> Error! (Attempt ' + attempt + ') Retrying...');
+                        // Retry after 1 second in case of an error
+                        setTimeout(sendRequest, 1000); // Retry after 1 second
+                    } else {
+                        $("#save_" + id).html('<span class="bi bi-x"></span> Error! Maximum attempts reached');
+                        reject(false); // Rejects the Promise with false after maximum attempts
+                    }
+                });
+        };
+
+        // Initial request
+        sendRequest();
+    });
+}
+
+async function upload_api(file, file_url, callback) {
+    //---
+    var api_url = 'auth.php';
+    //---
+    var formData = {
+        action: "api",
+        filename: file,
+        url: file_url,
+        comment: 'comment',
+        do: 'upload',
+    }
+    //---
+    var urlx = window.location.origin + '/ncc_to_c/' + api_url + '?' + $.param(formData);
+    //---
+    $.ajax({
+        async: true,
+        url: api_url,
+        data: formData,
+        type: "POST",
+        dataType: "json",
+        success: function (data) {
+            callback(null, data, urlx);
+        },
+        error: function (data) {
+            callback('Error occurred', data, urlx);
+        }
+    });
+}
+
+async function start_up(file, img_url, id) {
+    var idElement = $("#" + id);
+    idElement.html('<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Uploading..');
+    //---
+    await upload_api(file, img_url, function (err, data, urlx) {
+        //---
+        // { "error": { "code": "mwoauth-invalid-authorization", "info": "The authorization headers in your request are not valid: Invalid signature", "*": "" } }
+        var error = err;
+        //---
+        if (data.error) {
+            error = data.error;
+            if (data.error.code) {
+                error = data.error.code + ': ' + data.error.info;
+            }
+        }
+        //---
+        // {"upload":{"result":"Warning","warnings":{"was-deleted":"Z.jpg"},"filekey":"1ah474dii5sk.fjn2sw.13.","sessionkey":"1ah474dii5sk.fjn2sw.13."}}
+        if (data.upload != undefined) {
+            data = data.upload;
+        }
+        //---
+        console.log(urlx);
+        console.log(JSON.stringify(data));
+        //---
+        if (error) {
+            $("#error_" + id).show();
+            idElement_err(idElement, 'false: ' + error);
+        } else if (!data) {
+            idElement_err(idElement, 'false: no data');
+        } else {
+            var results = data.result;
+            var warnings = data.warnings;
+            if (results == "Success") {
+                $('#name_' + id).html('<a href="https://commons.wikimedia.org/wiki/File:' + file + '" target="_blank">' + file + '</a>');
+                idElement.text('true');
+                idElement.css({ "color": "#45f533", "font-weight": "bold" });
+            } else if (!results) {
+                console.log(data);
+                idElement_err(idElement, 'false, no results');
+            } else if (warnings) {
+                idElement_err(idElement, 'false, warnings: ' + JSON.stringify(data));
+            } else {
+                idElement_err(idElement, 'false, results: ' + results);
+            }
+        }
+    });
+
+}
+
+async function up_files() {
+
+    var to_up = document.getElementsByName('toup');
+
+    if (to_up.length == 0) {
+        $("#crop_logo").hide();
+        $("#crop_logo_err").show();
+        return;
+    }
+    // do get crop for to_up elements
+    for (var i = 0; i < to_up.length; i++) {
+        var id = to_up[i].getAttribute("idt");
+        var img_url = to_up[i].getAttribute("url");
+
+        var imagename = $("#name_" + id).text();
+        // var imagename = sessionStorage.getItem(id);
+
+        console.log("up: id:" + id + " imagename:" + imagename);
+        if (imagename != null && imagename != undefined && imagename != '') {
+            await start_up(imagename, img_url, id);
+        }
+    };
+}
